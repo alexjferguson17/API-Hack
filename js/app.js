6ead7455b11bfd816a27b86ef7f2c343ef1aec19
Alex Ferguson <alexjferguson17@gmail.com>
2016-02-19 14:29:12 -0500

    Pulling in all of the information using a template. I just need to style it now

--- js/app.js
+++ js/app.js
@@ -1,30 +1,53 @@
 $(document).ready(function(){
+
+  var requestData = function(queryVal){ //queryVal is renamed to data
+    console.log(queryVal);
+
+    $.ajax({ // ajax starts
+       url: "https://www.tastekid.com/api/similar",
+       jsonp: "callback",
+
+       data: {
+          q: queryVal,
+          k: "205150-Testapp-KCD81AJE",
+          info: 1,
+          limit: 5
+       },
+
+       dataType: "jsonp",
+
+       success: function(response) {
+          console.log(response);
+
+         $.each(response.Similar.Results, function(index, value){
+          //  $('#search-results').append(value.Name);
+          //  var url = 'https://i.ytimg.com/vi/' + value.yID + '/mqdefault.jpg'
+          //  var img = $('<img src="' + url + '">');
+          //  $('#search-results').append(img);
+          var information = showInspiration(value)
+          $('.search-results').append(information);
+         });
+      }
+    }); // ajax finishes
+
+  } // requestData finish
+
+  var showInspiration = function(value) {
+    var url = 'https://i.ytimg.com/vi/' + value.yID + '/mqdefault.jpg';
+    var result = $('.templates .information').clone();
+    var img = $('<img src="' + url + '">');
+    $('.typeImg').append(img);
+    result.find('.name').text(value.Name);
+    result.find('.catergory').text(value.Type);
+    result.find('.preview').text(value.wTeaser);
+
+    return result;
+  };
+
+  // submit starts
   $('#search-term').submit(function(event){
     event.preventDefault();
-    var query = $('#query').val();
-    requestData(query);
-  });
-
-  var requestData=function(data){console.log(data)};
-  // $.getJSON('?q=&&callback=myFunction')
-
-  $.ajax({
-     url: "https://www.tastekid.com/api/similar",
-
-     // The name of the callback parameter, as specified by the YQL service
-     jsonp: "callback",
-
-     data: {
-         q: "red+hot+chili+peppers",
-         k: "205150-Testapp-KCD81AJE"
-     },
-
-     // Tell jQuery we're expecting JSONP
-     dataType: "jsonp",
-
-         // Work with the response
-     success: function(response) {
-         console.log(response);
-     }
-  });
+    var queryVal = $('#query').val();
+    requestData(queryVal);
+  }); //submit finishes
 });
